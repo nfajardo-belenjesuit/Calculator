a8a1d5066378e9cc1aea883cc981748e6b869f4d
nfajardo-belenjesuit <89091708+nfajardo-belenjesuit@users.noreply.github.com>
2023-10-10 16:46:16 +0000

    Added subtract

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
 class Main {
-	  public static void main(String[] args) {
+	public static void main(String[] args) {
 	    
-	  }
+	}
 
 	public void systemCheck(){
 		System.out.println("System online");
+	}
+
+	public int subtract(int[] a){
+		int total = 0;
+		for(int num : a){
+			total -= num;
+		}
+		return total;
 	}
 }
