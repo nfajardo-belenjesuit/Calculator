799e97569f7ff2cb2bba9aac67c20244e9459d3b
nfajardo-belenjesuit <89091708+nfajardo-belenjesuit@users.noreply.github.com>
2023-10-10 16:35:51 +0000

    Added a system check to the repository.

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+	  public static void main(String[] args) {
+	    
+	  }
+
+	public void systemCheck(){
+		System.out.println("System online");
+	}
 }
